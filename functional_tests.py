867f527e40339a24f3ee7c37c9f79167c9f0e45b
leofontolan <leofontolan@gmail.com>
2022-03-22 16:58:48 -0300

    Functional Test Selenium

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,31 +18,46 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ela notou que o título da página e o cabeçalho mencionam to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		    # Ela é convidada a inserir uma tarefa diretamente
-		
-		
-		    # Ela digita "Comprar penas de pavão" em uma caixa de texto 
-		    # (o hobby de Edith é fazer iscas de fly-fishing)
-		
-		
-		    # Quando ela aperta enter, a página é atualizada e agora a página lista 
-		    # "1: Comprar penas de pavão" como um item em uma lista de tarefas
-		
-		    # Ainda há uma caixa de texto convidando-a a adicionar outro item. 
-		    # Ela escrever "Use as penas de pavão para fazer iscas" (Edith é muito metódica)
-		
-		
-		    # A página é atualizada novamente e agora mostra os dois itens da lista
-		
-		    # Edith se pergunta se o site lembrará de sua lista. 
-		    # Então ela vê que o site gerou uma URL única para ela
-		    # Existe um texto explicativo sobre isso.
-		
-		    # Ela visita essa URL - E sua lista de tarefas continua lá.
-		
-		    # Satisfeita, ela volta a dormir
+        # Ela é convidada a inserir uma tarefa diretamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+    
+        # Ela digita "Comprar penas de pavão" em uma caixa de texto 
+        # (o hobby de Edith é fazer iscas de fly-fishing)
+        inputbox.send_keys('Buy peacock feathers')
+    
+        # Quando ela aperta enter, a página é atualizada e agora a página lista 
+        # "1: Comprar penas de pavão" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+    
+        # Ainda há uma caixa de texto convidando-a a adicionar outro item. 
+        # Ela escrever "Use as penas de pavão para fazer iscas" (Edith é muito metódica)
+        self.fail('Finish the test!')
+    
+    
+        # A página é atualizada novamente e agora mostra os dois itens da lista
+    
+        # Edith se pergunta se o site lembrará de sua lista. 
+        # Então ela vê que o site gerou uma URL única para ela
+        # Existe um texto explicativo sobre isso.
+    
+        # Ela visita essa URL - E sua lista de tarefas continua lá.
+    
+        # Satisfeita, ela volta a dormir
 
 if __name__ == '__main__':
     unittest.main()
